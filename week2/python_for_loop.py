cf2f9b2bc022e23a75442e76536651f78e4344da
UsrDrn08 <bioptic607@gmail.com>
2026-02-06 23:44:33 +0500

    Add files via upload

---
+++ week2/python_for_loop.py
@@ -0,0 +1,21 @@
+# Перебор списка
+for x in [1, 2, 3]:
+    print(x)
+
+# range()
+for i in range(5):
+    print(i)
+
+# range с шагом
+for i in range(0, 10, 2):
+    print(i)
+
+# Перебор строки
+for char in "Python":
+    print(char)
+
+# for + else
+for i in range(3):
+    print(i)
+else:
+    print("Цикл завершён")
